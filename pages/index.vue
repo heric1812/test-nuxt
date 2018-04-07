43e9ba1c0bff2b9b61a24dda337ecfbd4040b2f7
heric1812 <heric1812@gmail.com>
2018-04-07 12:13:51 -0400

    tester

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,7 @@
       <h2 class="subtitle">Blog project</h2>
       <div class="links">
         <nuxt-link to="/about" class="button--green">About</nuxt-link>
-        <!-- <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a> -->
+        <nuxt-link to="/users" class="button--green">Users</nuxt-link>
       </div>
     </div>
   </section>
---
+++ pages/users/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h3>Users List</h3>
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/users/one">Users One</nuxt-link>
+    <nuxt-link to="/about">About</nuxt-link>
+    <nuxt-link to="/not-exist">Route not exist</nuxt-link>
+  </div>
+</template>
