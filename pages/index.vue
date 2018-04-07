a7db3f7503770d143dd9a3d34ae30d5ee937e0c6
heric1812 <heric1812@gmail.com>
2018-04-07 11:43:56 -0400

    changes

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,18 @@
   <section class="container">
     <div>
       <app-logo/>
-      <h1 class="title">
-        vue-nuxt-example
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
+      <h1 class="title">Test Nuxt</h1>
+      <h2 class="subtitle">Blog project</h2>
       <div class="links">
-        <a
+        <nuxt-link to="/about" class="button--green">About</nuxt-link>
+        <!-- <a
           href="https://nuxtjs.org/"
           target="_blank"
           class="button--green">Documentation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey">GitHub</a>
+          class="button--grey">GitHub</a> -->
       </div>
     </div>
   </section>
@@ -26,9 +23,7 @@
 import AppLogo from '~/components/AppLogo.vue'
 
 export default {
-  components: {
-    AppLogo
-  }
+  components: { AppLogo }
 }
 </script>
 
