d2476f6519cbd1bf8f8ca1f8f98cc39db644876a
heric1812 <heric1812@gmail.com>
2018-04-07 12:17:52 -0400

    deploy failed

---
+++ pages/users/one.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h3>User One</h3>
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/users">Users</nuxt-link>
+    <nuxt-link to="/about">About</nuxt-link>
+  </div>
+</template>
